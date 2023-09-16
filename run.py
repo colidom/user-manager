caa0606c84aaf289939f37b546e2586e1e3c7a00
Carlos Oliva <carlos.oliva@eviden.com>
2023-09-16 16:17:20 +0200

    Modo terminal e interfaz

--- run.py
+++ run.py
@@ -1,4 +1,19 @@
+import ui
+import sys
 import menu
 
 if __name__ == "__main__":
-    menu.launch()
+    if len(sys.argv) > 1 and sys.argv[1] == "-t":
+        menu.launch()
+    elif len(sys.argv) > 1 and sys.argv[1] == "-i":
+        app = ui.MainWindow()
+        app.mainloop()
+    else:
+        print(
+            """
+Wrong parameter use it as below:
+    python run.py -<arg>
+    -t Terminal version
+    -i Interface version
+"""
+        )
